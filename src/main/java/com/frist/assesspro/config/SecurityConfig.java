94b81145caf7be91f66de9ee80082b290d4b6c32
Vadim <vadim-123455@mail.ru>
2026-01-27 19:45:12 +0300

    c3

--- src/main/java/com/frist/assesspro/config/SecurityConfig.java
+++ src/main/java/com/frist/assesspro/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.frist.assesspro.config;
 
-import com.frist.assesspro.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -22,38 +26,38 @@ public class SecurityConfig  {
     }
 
 
-//    @Bean
-//    public AuthenticationManager authenticationManager(
-//            AuthenticationConfiguration authConfig) throws Exception {
-//        return authConfig.getAuthenticationManager();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) {
-//        http.csrf(AbstractHttpConfigurer::disable) // Для упрощения, можно включить позже
-//                .authorizeHttpRequests(auth -> auth
-//                        // Публичные эндпоинты
-//                        .requestMatchers("/", "/home", "/login","/password-check","/register").permitAll()
-//                        // Только для создателей тестов
-//                        .requestMatchers("/creator/**").hasRole("CREATOR")
-//                        .requestMatchers("/admin/**").hasRole("ADMIN")
-//                        // Только для тестирующих
-//                        .requestMatchers("/user/**").hasRole("USER")
-//                        // Все остальные запросы требуют аутентификации
-//                        .anyRequest().authenticated())
-//                .formLogin(form -> form
-//                        .loginPage("/login")
-//                        .loginProcessingUrl("/login")
-//                        .defaultSuccessUrl("/dashboard", true)
-//                        .failureUrl("/login?error=true")
-//                        .usernameParameter("username")
-//                        .passwordParameter("password")
-//                        .permitAll()
-//                )
-//                .exceptionHandling(exceptions -> exceptions
-//                        .accessDeniedPage("/access-denied")
-//                );
-//
-//        return http.build();
-//    }
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+        http.csrf(AbstractHttpConfigurer::disable) // Для упрощения, можно включить позже
+                .authorizeHttpRequests(auth -> auth
+                        // Публичные эндпоинты
+                        .requestMatchers("/", "/home", "/login","/password-check","/register").permitAll()
+                        // Только для создателей тестов
+                        .requestMatchers("/creator/**").hasRole("CREATOR")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        // Только для тестирующих
+                        .requestMatchers("/user/**").hasRole("USER")
+                        // Все остальные запросы требуют аутентификации
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/dashboard", true)
+                        .failureUrl("/login?error=true")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .permitAll()
+                )
+                .exceptionHandling(exceptions -> exceptions
+                        .accessDeniedPage("/access-denied")
+                );
+
+        return http.build();
+    }
 }
